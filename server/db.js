158ce1cb65ea903577210c6e5d3b3433dbc59df4
connergreene <csg1922@gmail.com>
2016-12-07 13:59:41 -0500

    changed uri

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var Promise = require('bluebird'); 
 Promise.promisifyAll(mongoose);
 
-var databaseURI = 'mongodb://127.0.0.1:27017/shwayter';
+var databaseURI = 'mongodb://heroku_96pfxt6w:mhhq6ohhk4k4c16ht12fuv0749@ds159237.mlab.com:59237/heroku_96pfxt6w';
 
 var db = mongoose.connect(databaseURI).connection;
 
