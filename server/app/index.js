204a1d6d3999a676c1df60cb5a66bf777b1c58a9
connergreene <csg1922@gmail.com>
2016-12-09 03:38:56 -0500

    testing webhooks

--- server/app/index.js
+++ server/app/index.js
@@ -59,7 +59,7 @@ var options = {
       'Accept' : 'application/json',
       'Content-Type' : 'application/json'
     },
-    formData: postData
+    formData: JSON.stringify(postData)
 }
 request.put(options, function(error, res, body) {
     if (error) {
