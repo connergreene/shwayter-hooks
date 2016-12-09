9a15a354ad1cd950aaffff380c33841b1c21732c
connergreene <csg1922@gmail.com>
2016-12-09 02:20:38 -0500

    testing webhooks

--- server/app/index.js
+++ server/app/index.js
@@ -59,7 +59,7 @@ var options = {
       'Accept' : 'application/json',
       'Content-Type' : 'application/json'
     },
-    body: JSON.stringify(postData)
+    body: ["PAYMENT_UPDATED"]
 }
 request.put(options, function(error, res, body) {
     if (error) {
