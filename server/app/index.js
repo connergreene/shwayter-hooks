76bef6766faeb86e926887680052fb31fb5204fe
connergreene <csg1922@gmail.com>
2016-12-13 12:10:44 -0500

    testing webhooks

--- server/app/index.js
+++ server/app/index.js
@@ -59,7 +59,7 @@ var options = {
       'Accept' : 'application/json',
       'Content-Type' : 'application/json'
     },
-    body : ["PAYMENT_UPDATED"]
+    body : "[\"PAYMENT_UPDATED\"]"
 }
 request.put(options, function(error, res, body) {
     if (error) {
