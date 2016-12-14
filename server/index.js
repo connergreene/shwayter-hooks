8af095f76dd5ecd938f25441b03c88c685765568
connergreene <csg1922@gmail.com>
2016-12-14 16:19:15 -0500

    working on server side socket

--- server/index.js
+++ server/index.js
@@ -1,28 +1,15 @@
 'use strict';
 
-var chalk = require('chalk');
-var startDb = require('./db');
-var server = require('http').createServer();
+var app = require('./app'),
+	db = require('./db');
 
+var http = require('http');
+var httpServer = http.Server(app);
+//var io = require('socket.io')(http);
 
-var createApplication = function () {
-    var app = require('./app');
-    server.on('request', app);
-};
-
-var startServer = function () {
-
-    var PORT = process.env.PORT || 1337;
-
-    server.listen(PORT, function () {
-        console.log(chalk.blue('Server started on port', chalk.magenta(PORT)));
-    });
-
-};
-
-startDb.then(createApplication).then(startServer).catch(function (err) {
-    console.error(chalk.red(err.stack));
-    process.kill(1);
+var port = 8080;
+var server = httpServer.listen(process.env.PORT || port, function () {
+	console.log('HTTP server patiently listening on port', port);
 });
 
-module.exports = server;
+module.exports = httpServer;
