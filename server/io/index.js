4d78ce4722f91dece5c223d928fc27dbdb53e13b
connergreene <csg1922@gmail.com>
2016-12-14 14:35:25 -0500

    working on server side socket

---
+++ server/io/index.js
@@ -0,0 +1,19 @@
+'use strict';
+var socketio = require('socket.io');
+var mongoose = require('mongoose');
+var User = mongoose.model('Users');
+var io = null;
+
+module.exports = function (server) {
+
+    if (io) return io;
+
+    io = socketio(server);
+
+    io.on('connection', function (socket) {
+        console.log("socket connected");
+    });
+
+    return io;
+
+};
