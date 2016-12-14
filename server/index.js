f408b3b081b73c3f777b2cda08d38100f4abdbba
connergreene <csg1922@gmail.com>
2016-12-14 13:08:03 -0500

    changing server set up

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ var server = require('http').createServer();
 var createApplication = function () {
     var app = require('./app');
     server.on('request', app); // Attach the Express application.
-    require('./io')(server);   // Attach socket.io.
+    require('./socket.io')(server);   // Attach socket.io.
 };
 
 var startServer = function () {
