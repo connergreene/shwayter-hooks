d05930256546f55ddf9418090d40b85b903500c7
connergreene <csg1922@gmail.com>
2016-12-14 18:20:33 -0500

    testing socket

--- server/index.js
+++ server/index.js
@@ -7,15 +7,12 @@ var httpServer = require('http').Server(app);
 var io = require('socket.io')(httpServer);
 
 
-app.post('/order', function (req, res) {
-  // We're using Socket IO to communicate server --> client
-  // I think it's probably the best approach. There's also https://pusher.com/, but I think that's mainly used with Ruby/Python backends where sockets aren't as easy to use (it's easier to use Sockets in Node, especially with the SocketIO library)
-
-  // I hardcoded { item: 'coffee' } in. In the real app, perhaps Square gives you info
-  // on the order item? Or maybe it'll just give you an order item id, and you'll have to
-  // use the id to look up more info.
-  io.emit('order', { item: 'coffee' });
+io.on('connection', function(socket){
+	console.log("socket connected")
+	socket.emit('order', { item: 'coffee' });
 });
+
+
 
 var port = 8080;
 var server = httpServer.listen(process.env.PORT || port, function () {
