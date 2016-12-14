0db8139e4cf5108e135888fe4d5294e356408245
connergreene <csg1922@gmail.com>
2016-12-14 00:05:56 -0500

    sending requests to front end

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ var app = require('./app'),
 	db = require('./db');
 
 var port = 8080;
+console.log("thos os process: ", process.env)
 var server = app.listen(process.env.PORT || port, function () {
 	console.log('HTTP server patiently listening on port', port);
 });
