4bb880f312551b2abe978340ba0e0e72d3c35ae2
connergreene <csg1922@gmail.com>
2016-12-14 00:12:00 -0500

    sending requests to front end

--- browser/app/home/home.state.js
+++ browser/app/home/home.state.js
@@ -6,7 +6,8 @@ app.config(function ($stateProvider) {
 		templateUrl: '/browser/app/home/home.html',
 		controller: function($scope, $http, orderFactory){
 			if (!window.io) throw new Error('socket.io not found!');
-    		var socket = window.io('http://localhost:8080');
+			console.log(process.env.PORT)
+    		//var socket = window.io('http://localhost:8080');
 			//var socket = io('http://localhost:8080');
 			socket.on('order', function (order) {
     			$('ol').append($('<li>' + order + '</li>'));
