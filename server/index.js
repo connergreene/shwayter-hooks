7e509570fa2bcf830ecd0df1cd5ea4056588a1c9
connergreene <csg1922@gmail.com>
2016-12-14 00:09:14 -0500

    sending requests to front end

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ var app = require('./app'),
 
 //var port = 8080;
 var server = app.listen(process.env.PORT, function () {
-	console.log('HTTP server patiently listening on port', port);
+	console.log('HTTP server patiently listening on port', process.env.PORT);
 });
 
 module.exports = server;
