5a487ca331d116208dc649d668ee7aa0315b8273
connergreene <csg1922@gmail.com>
2016-12-15 11:57:41 -0500

    viewing json crashed

--- server/app/index.js
+++ server/app/index.js
@@ -108,7 +108,6 @@ app.post('/events', function(req, res, next){
         }
         //this is where it sends to front end
         console.log("it actually happened", body)
-        r.json(body);
         io.emit('order', body);
       });
   }
