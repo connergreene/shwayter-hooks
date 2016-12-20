1e69c261f484ce080126b2e804da0aae328709aa
connergreene <csg1922@gmail.com>
2016-12-20 14:18:08 -0500

    ticket color

--- browser/components/tickets/ticket.directive.js
+++ browser/components/tickets/ticket.directive.js
@@ -8,23 +8,20 @@ app.directive('ticket', function ($state, $location, $interval, Auth) {
 		link: function (scope) {
 			scope.ms = 0
 			scope.date = new Date();
-			scope.reset = function(){
-				scope.ms = 0;
-			};
 			$interval(function(){
 				scope.date = new Date();
 				scope.ms+=1000;
+				if(scope.ms <= 3000){
+					document.querySelector('.ticket').style.background = 'green';
+				}
+				else if(scope.ms > 3000 && scope.ms <= 6000){
+					document.querySelector('.ticket').style.background = 'orange';
+				}
+				else{
+					document.querySelector('.ticket').style.background = 'red';
+				}
 			},1000)
-			console.log(scope.ms)
-			if(scope.ms <= 3000){
-				document.querySelector('.ticket').style.background = 'green';
-			}
-			else if(scope.ms > 3000 && scope.ms <= 6000){
-				document.querySelector('.ticket').style.background = 'orange';
-			}
-			else{
-				document.querySelector('.ticket').style.background = 'red';
-			}
+			
 			console.log("this is order", scope.order);
 		}
 	}
