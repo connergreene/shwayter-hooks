3d0022858b6f4d02da140764685b0af5f36597ff
connergreene <csg1922@gmail.com>
2016-12-21 14:24:29 -0500

    trying to fix colors

--- browser/components/tickets/ticket.directive.js
+++ browser/components/tickets/ticket.directive.js
@@ -9,18 +9,18 @@ app.directive('ticket', function ($state, $location, $interval, Auth) {
 		link: function (scope, element, attrs) {
 			scope.ms = 0
 			scope.date = new Date();
+			scope.timer = {green:true, orange: false, red: false}
 			var time = $interval(function(){
 				scope.date = new Date();
 				scope.ms+=1000;
-				// if(scope.ms <= 3000){
-				// 	document.querySelector('.ticket').style.background = 'green';
-				// }
-				// else if(scope.ms > 3000 && scope.ms <= 6000){
-				// 	document.querySelector('.ticket').style.background = 'orange';
-				// }
-				// else{
-				// 	document.querySelector('.ticket').style.background = 'red';
-				// }
+				if(scope.ms > 3000 && scope.ms <= 6000){
+					scope.timer.green = false;
+					scope.timer.orange = true;
+				}
+				else if (scope.ms > 6000){
+					scope.timer.orange = false;
+					scope.timer.red = true;
+				}
 			},1000)
 
 			scope.remove = function() {
