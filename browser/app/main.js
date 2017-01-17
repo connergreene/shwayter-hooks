09a1717d6b898004f76f7b4dad4ffd85eb9edff7
connergreene <csg1922@gmail.com>
2017-01-16 20:48:07 -0500

    changing session strategy

--- browser/app/main.js
+++ browser/app/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('shwayter', ['ui.router']);
+var app = angular.module('shwayter', ['ui.router', 'ngCookies']);
 
 app.config(function ($urlRouterProvider, $locationProvider) {
 	$locationProvider.html5Mode(true);
@@ -13,9 +13,43 @@ app.config(function ($urlRouterProvider, $locationProvider) {
 
 });
 
-app.run(function(Auth){
+app.run(function($rootScope, Auth, $state){
   // re retrieve user from backend 
   // every time the user refreshes the page
-  console.log('in run block, getting the user') 
-  Auth.refreshCurrentUser();
+  // console.log('in run block, getting the user') 
+  // Auth.refreshCurrentUser();
+	function preventStateChange (message, redirect) {
+		if (redirect) {
+			$state.go(redirect);
+		}
+		else {
+			$state.go('home');
+		}
+	}
+	
+
+	$rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, Auth) {
+		if (typeof toState.authenticate === 'undefined') {
+			return;
+		}
+		console.log("auth2", Auth)
+		Auth
+		.getCurrentUser()
+		.then(function (currentUser) {
+			var isLoggedIn = !!currentUser._id;
+
+			var isAuthorized = isLoggedIn && currentUser._id.toString() === toParams.id;
+
+			if (toState.authenticate.loggedOut) { // this route requires you to be logged out
+				if (isLoggedIn) {
+					preventStateChange("You're logged in.");
+				}
+			}
+			else if (!isLoggedIn) {
+				preventStateChange('Must be logged in to access this route.', 'login');
+			}
+		})
+	});
 })
+
+
