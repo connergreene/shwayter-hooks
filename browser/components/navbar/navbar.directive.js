969a0c2911c5939e749cfee55dfa3efa438b0ffd
connergreene <csg1922@gmail.com>
2017-01-19 16:09:15 -0500

    hide navbar on login
    hide navbar on login
    
    hide navbar on login
    
    hide navbar on login
    
    hide navbar on login
    
    hide navbar on login
    
    hide navbar on login
    
    hide navbar on login
    
    hide navbar on login
    
    hide navbar on login
    
    hide navbar on login
    
    hide navbar on login
    
    hide navbar on login

--- browser/components/navbar/navbar.directive.js
+++ browser/components/navbar/navbar.directive.js
@@ -10,6 +10,16 @@ app.directive('navbar', function ($state, $location, Auth) {
 				var path = $location.path();
 				return path.startsWith(partial);
 			};
+
+			scope.isHome = function(){
+				if ($location.path() === '/'){
+					return true;
+				}
+				else{
+					return false;
+				}
+			}
+			
 			scope.isLoggedIn = function () {
 				return Boolean(Auth.getCurrentUser());
 			};
