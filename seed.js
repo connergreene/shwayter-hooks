e20eae52cb9b3b1ae3fc36a612ab5770a82e353a
connergreene <csg1922@gmail.com>
2017-01-25 14:02:37 -0500

    changing seeding

--- seed.js
+++ seed.js
@@ -7,28 +7,6 @@ var chance = require('chance')(123),
 var db = require('./server/db');
 var User = require('./server/api/users/user.model');
 
-
-db.drop = Promise.promisify(db.db.dropDatabase.bind(db.db));
-db.drop();
-var users = [
-    {
-        name: 'Conner Greene',
-        email: 'csg1922@gmail.com',
-        password: 'shnoops',
-        isAdmin: true
-    },
-    {
-        name: 'Ivan Greene',
-        email: 'ivan@pudgeknuckles.com',
-        password: 'shnoops',
-        isAdmin: true
-    }
-];
-
-return User.create(users)
-
-
-
 // function randUser () {
 //     return new User({
 //         name: [chance.first(), chance.last()].join(' '),
@@ -40,47 +18,47 @@ return User.create(users)
 //     });
 // }
 
-// function generateAll () {
-//     var users = [];
-//     users.push(
-//         new User({
-//             name: 'Conner Greene',
-//             email: 'csg1922@gmail.com',
-//             password: 'shnoops',
-//             isAdmin: true
-//         }),
-//         new User({
-//             name: 'Ivan Greene',
-//             email: 'ivan@pudgeknuckles.com',
-//             password: 'shnoops',
-//             isAdmin: true
-//         })
-//     );
+function generateAll () {
+    var users = [];
+    users.push(
+        new User({
+            name: 'Conner Greene',
+            email: 'csg1922@gmail.com',
+            password: 'shnoops',
+            isAdmin: true
+        }),
+        new User({
+            name: 'Ivan Greene',
+            email: 'ivan@pudgeknuckles.com',
+            password: 'shnoops',
+            isAdmin: true
+        })
+    );
 
-//     return users;
-// }
+    return users;
+}
 
-// function seed () {
-//     var docs = generateAll();
-//     return Promise.map(docs, function (doc) {
-//         return doc.save();
-//     });
-// }
+function seed () {
+    var docs = generateAll();
+    return Promise.map(docs, function (doc) {
+        return doc.save();
+    });
+}
 
-// db.drop = Promise.promisify(db.db.dropDatabase.bind(db.db));
+db.drop = Promise.promisify(db.db.dropDatabase.bind(db.db));
 
-// db.on('open', function () {
-//     db.drop()
-//     .then(function () {
-//         return seed();
-//     })
-//     .then(function () {
-//         console.log('Seeding successful');
-//     }, function (err) {
-//         console.error('Error while seeding');
-//         console.error(err.stack);
-//     })
-//     .then(function () {
-//         process.exit();
-//     });
-// });
+db.on('open', function () {
+    db.drop()
+    .then(function () {
+        return seed();
+    })
+    .then(function () {
+        console.log('Seeding successful');
+    }, function (err) {
+        console.error('Error while seeding');
+        console.error(err.stack);
+    })
+    .then(function () {
+        process.exit();
+    });
+});
