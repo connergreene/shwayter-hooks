858add303ecf9a97cce63f4114b2f3122f5e20d2
connergreene <csg1922@gmail.com>
2017-01-25 17:26:26 -0500

    testing current user

--- browser/utils/auth.factory.js
+++ browser/utils/auth.factory.js
@@ -7,6 +7,7 @@ app.factory('Auth', function ($http, $cookies, $q, $rootScope, Session, AUTH_EVE
   function onSuccessfulLogin(response) {
       var data = response.data;
       console.log("this is data:", data);
+      console.log("session id:", data.id);
       Session.create(data.id, data.user);
       $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
       return data.user;
