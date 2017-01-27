6c645d9897ff45b408b7497bdbf0dc7044131a61
connergreene <csg1922@gmail.com>
2017-01-27 17:24:30 -0500

    hid db uri

--- server/db.js
+++ server/db.js
@@ -6,10 +6,6 @@ var chalk = require('chalk');
 
 Promise.promisifyAll(mongoose);
 
-var databaseURI = 'mongodb://heroku_96pfxt6w:mhhq6ohhk4k4c16ht12fuv0749@ds159237.mlab.com:59237/heroku_96pfxt6w';
-
-
-
 var db = mongoose.connect(process.env['MONGODB_URI']).connection;
 
 var startDbPromise = new Promise(function (resolve, reject) {
@@ -19,13 +15,6 @@ var startDbPromise = new Promise(function (resolve, reject) {
 
 console.log(chalk.yellow('Opening connection to MongoDB . . .'));
 startDbPromise.then(function () {
-	if (databaseURI === process.env['MONGODB_URI']){
-		console.log("they are the same")
-	}
-	else{
-		console.log(databaseURI);
-		console.log(process.env['MONGODB_URI'])
-	}
     console.log(chalk.green('MongoDB connection opened!'));
 });
 
