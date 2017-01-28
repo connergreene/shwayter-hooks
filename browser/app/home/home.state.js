6b64b7db5809f69c32040959b30f5013eab3e389
connergreene <csg1922@gmail.com>
2017-01-28 14:28:13 -0500

    notification

--- browser/app/home/home.state.js
+++ browser/app/home/home.state.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.config(function ($stateProvider) {
+app.config(function ($stateProvider, $rootScope) {
 	$stateProvider.state('home', {
 		url: '/kds',
 		templateUrl: '/browser/app/home/home.html',
@@ -13,6 +13,7 @@ app.config(function ($stateProvider) {
 					$scope.$index++;
 				});
  			});
+ 			console.log("title", $rootScope.title)
 		},
 		authenticate: {
 			loggedOut: false  //required to be logged in
