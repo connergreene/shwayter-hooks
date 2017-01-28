28041f066b8ae87692fbf06d9f559797248cf3b9
connergreene <csg1922@gmail.com>
2017-01-28 15:04:49 -0500

    notification

--- browser/app/home/home.state.js
+++ browser/app/home/home.state.js
@@ -7,20 +7,19 @@ app.config(function ($stateProvider) {
 		controller: function($scope, $http, $state, $rootScope, Auth){
 			var socket = io.connect();
 			$scope.tickets = [];
-			$scope.counter;
+			$scope.counter=0;
 			socket.on('order', function (order) {
 				$scope.counter++;
 				$scope.$apply(function(){
 					$scope.tickets.push(order);
+					if($scope.counter === 0){
+		 				document.title = "Shwayter"
+		 			}
+		 			else{
+		 				document.title = "Shwayter(" + $scope.counter + ")";
+		 			}
 				});
  			});
- 			console.log($scope.counter)
-			if($scope.counter === 0){
- 				document.title = "Shwayter"
- 			}
- 			else{
- 				document.title = "Shwayter(" + $scope.counter + ")";
- 			}
 		},
 		authenticate: {
 			loggedOut: false  //required to be logged in
